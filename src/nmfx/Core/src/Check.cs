d261c194ca57291f614c521f33c1df489d7f5c4d
Michael Herndon <mherndon@nerdymishka.com>
2021-11-11 21:33:08 -0600

    GH-3 remove shell projects, add Check class

---
+++ src/nmfx/Core/src/Check.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Threading;
+
+namespace NerdyMishka.Core
+{
+    public static class Check
+    {
+        public static T NotNull<T>([NotNullIfNotNull("value")] T? value, string parameterName)
+        {
+            if (value is null)
+                throw new ArgumentNullException(parameterName);
+
+            return value;
+        }
+
+        public static string NotNullOrEmpty([NotNullIfNotNull("value")] string? value, string parameterName)
+        {
+            if (IsNullOrEmpty(value))
+                throw new ArgumentNullException(parameterName, $"{parameterName} must not be null or empty.");
+
+            return value;
+        }
+
+        public static string NotNullOrWhiteSpace([NotNullIfNotNull("value")] string? value, string parameterName)
+        {
+            if (IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException(parameterName, $"{parameterName} must not be null or empty.");
+
+            return value;
+        }
+
+        public static bool IsNullOrEmpty([NotNullWhen(false)] string? value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrWhiteSpace([NotNullWhen(false)] string? value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
